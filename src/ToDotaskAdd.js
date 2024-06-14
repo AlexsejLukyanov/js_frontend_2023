2c1cce910ac087de2410c6e0dd6069d5f6be8b9f
AlexsejLukyanov <aleksej.lukyanov.02@mail.ru>
2024-06-14 20:12:38 +0300

    Apply custom UI template

--- src/ToDotaskAdd.js
+++ src/ToDotaskAdd.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
 
+import { todoAdd } from './actions'
 
 class ToDotaskAddInner extends React.Component {
+	
 	constructor(props) {
 		super(props);
 		
@@ -14,7 +17,6 @@ class ToDotaskAddInner extends React.Component {
 		this.onNameChange = this.onNameChange.bind(this);
 		this.onDescriptionChange = this.onDescriptionChange.bind(this);
 		this.onAddFormSubmit = this.onAddFormSubmit.bind(this);
-		console.log(this.props.onTaskAdd);
 	}
 		
 	onNameChange(e) {
@@ -49,26 +51,40 @@ class ToDotaskAddInner extends React.Component {
 		}).then((res) => {
 			return res.json();
 		}).then((data) => {
-			this.props.onTaskAdd(data);
-			this.props.history('/')
+			this.props.dispatch(todoAdd(data._id, data.name, data.description));
+			this.props.history('/');
 		});
 	}
 	
 	render() {
 		return (
-			<form onSubmit={this.onAddFormSubmit}>
-				<input type="text" value={this.state.name} onChange={this.onNameChange} placeholder="name"/>
-				<input type="text" value={this.state.description} onChange={this.onDescriptionChange} placeholder="description"/>
-				<input type="submit" value="Add" />
-			</form>
+			<div className="card-header-shadow-2x mb-3 card">
+				<div className="card-header-tab card-header">
+					<div className="card-header-title font-size-lg text-capitalize font-weight-normal">
+						<i className="fa fa-tasks"></i>&nbsp;Add Task
+					</div>
+				</div>
+				<form onSubmit={this.onAddFormSubmit}>
+				<div className="widget-content">
+					<div className="widget-content-wrapper">
+						<input type="text" value={this.state.name} onChange={this.onNameChange} placeholder="Name" className="form-control"/>
+						<input type="text" value={this.state.description} onChange={this.onDescriptionChange} placeholder="Description" className="form-control"/>
+						<input type="submit" value="Add" className="btn btn-primary" />
+					</div>
+				</div>
+				</form>
+				<div className="d-block text-right card-footer">
+					<NavLink to='/'className="btn btn-primary">Back to list</NavLink>
+				</div>
+			</div>
 		)
 	}
 }
 
-const ToDotaskAdd= (props) =>{
+const ToDotaskAdd = (props) => {
 	return (
 		<ToDotaskAddInner {...props} history={useNavigate()} />
 	)
 }
 
-export default ToDotaskAdd;
+export default connect()(ToDotaskAdd);
